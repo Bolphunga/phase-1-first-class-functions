a6d69bfb1cd8f0984385695f8d487bd6d2e4bdda
Bolphunga <aymendj@yahoo.com>
2023-07-28 19:44:07 +0200

    Completed assignment

---
+++ index.js
@@ -0,0 +1,90 @@
+function receivesAFunction(callback) {
+    
+callback()
+}
+
+function returnsANamedFunction() {
+    return function fn(){}
+}
+
+function returnsAnAnonymousFunction() {
+    return () => {}
+}
+
+
+
+
+
+
+// function Monday() {
+//     console.log("Go for a five-mile run")
+// }   console.log("Pump iron")
+
+// function Tuesday() {
+//     console.log("Go for a five-mile run");
+//     console.log("Swim 40 laps");
+//   }
+  
+//   function Wednesday() {
+//     console.log("Go for a five-mile run");
+//     console.log("Go for a five-mile run");
+//   }
+  
+//   function Thursday() {
+//     console.log("Go for a five-mile run");
+//     console.log("Pump iron");
+//   }
+  
+//   function Friday() {
+//     console.log("Go for a five-mile run");
+//     console.log("Swim 40 laps");
+//   }
+
+//   function runFiveMiles() {
+//     console.log("Go for a five-mile run");
+//   }
+
+//   function liftWeights() {
+//     console.log("Pump iron");
+//   }
+  
+//   function swimFortyLaps() {
+//     console.log("Swim 40 laps");
+//   }
+
+//   function Monday() {
+//     runFiveMiles();
+//     liftWeights();
+//   }
+
+//   function exerciseRoutine(postRunActivity) {
+//     runFiveMiles();
+//     postRunActivity();
+//   }
+
+//   function Monday(exerciseRoutine) {
+//     exerciseRoutine(liftWeights);
+//   }
+
+//   function exerciseRoutine(postRunActivity) {
+//     runFiveMiles();
+//     postRunActivity();
+//   }
+
+//   function morningRoutine(exercise) {
+//     let breakfast;
+    
+//     if (exercise === liftWeights) {
+//         breakfast = 'Proteinbar';
+//     } else if (exercise === swimFortyLaps) {
+//         breakfast = "Kale smoothie"
+//     } else {
+//         breakfast = 'Granola'
+//     }
+
+//     exerciseRoutine(exercise);
+//     return function() {console.log(`Nom nom nom, this ${breakfast} is delicious`);}
+//   }
+
+
+  
